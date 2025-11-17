b1eefe545722c2e9f10e6c5b019fbd224c8266f5
Jose Mena <jmena@rccl.com>
2025-11-17 02:22:27 -0500

    fix: conditionally import spec only if available

--- ios/ReactNativeBrownfieldModule.h
+++ ios/ReactNativeBrownfieldModule.h
@@ -2,7 +2,7 @@
 
 #import <React/RCTBridgeModule.h>
 
-#if RCT_NEW_ARCH_ENABLED
+#if RCT_NEW_ARCH_ENABLED && __has_include(<ReactNativeBrownfieldSpec/ReactNativeBrownfieldSpec.h>)
 #import <ReactNativeBrownfieldSpec/ReactNativeBrownfieldSpec.h>
 @interface ReactNativeBrownfieldModule : NSObject <NativeReactNativeBrownfieldModuleSpec>
 #else
