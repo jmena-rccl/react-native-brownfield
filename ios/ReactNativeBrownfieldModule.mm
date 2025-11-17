f8404be655737541027a34223a876123331b1d15
Jose Mena <jmena@rccl.com>
2025-11-17 02:23:48 -0500

    fix: conditionally compile TurboModule method only when spec is available

--- ios/ReactNativeBrownfieldModule.mm
+++ ios/ReactNativeBrownfieldModule.mm
@@ -18,7 +18,7 @@ RCT_EXPORT_METHOD(popToNative:(BOOL)animated) {
   [ReactNativeBrownfieldModuleImpl popToNativeWithAnimated:animated];
 }
 
-#if RCT_NEW_ARCH_ENABLED
+#if RCT_NEW_ARCH_ENABLED && __has_include(<ReactNativeBrownfieldSpec/ReactNativeBrownfieldSpec.h>)
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params {
   return std::make_shared<facebook::react::NativeReactNativeBrownfieldModuleSpecJSI>(params);
 }
