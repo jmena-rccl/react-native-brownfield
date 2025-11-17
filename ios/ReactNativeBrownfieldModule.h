8d8851b3a8a11017b035b5b4059380575249c11e
Jose Mena <jmena@rccl.com>
2025-11-17 02:09:56 -0500

    fix: use forward declaration in header to avoid circular dependency

--- ios/ReactNativeBrownfieldModule.h
+++ ios/ReactNativeBrownfieldModule.h
@@ -1,10 +1,6 @@
 #ifdef __cplusplus
 
-#if __has_include("ReactBrownfield-Swift.h")
-#import "ReactBrownfield-Swift.h"
-#else
-#import <ReactBrownfield/ReactBrownfield-Swift.h>
-#endif
+@protocol NativeReactNativeBrownfieldModuleSpec;
 
 @interface ReactNativeBrownfieldModule : NSObject <NativeReactNativeBrownfieldModuleSpec>
 @end
