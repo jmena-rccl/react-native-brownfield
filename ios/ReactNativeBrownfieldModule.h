a81c59dcac93f4e03069cdcf6035587379bbd5b1
Jose Mena <jmena@rccl.com>
2025-11-17 02:07:51 -0500

    fix: add conditional import for Swift bridging header

--- ios/ReactNativeBrownfieldModule.h
+++ ios/ReactNativeBrownfieldModule.h
@@ -1,6 +1,10 @@
 #ifdef __cplusplus
 
+#if __has_include("ReactBrownfield-Swift.h")
 #import "ReactBrownfield-Swift.h"
+#else
+#import <ReactBrownfield/ReactBrownfield-Swift.h>
+#endif
 
 @interface ReactNativeBrownfieldModule : NSObject <NativeReactNativeBrownfieldModuleSpec>
 @end
